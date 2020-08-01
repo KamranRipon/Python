912b112a035b1cf327ee96b6b3e7015c08269027
akash062 <ripon_aiub@yahoo.com>
2020-08-01 23:24:16 +0200

    lambda function

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,1 +1,12 @@
+people = [
+    {"name":"Harry","house":"Gryffindor"},
+    {"name":"Cho","house":"Ravenchlow"},
+    {"name":"Draco","house":"Slytherin"}
+    
+    ]
 
+
+
+people.sort(key=lambda person: person["name"])
+
+print(people)
